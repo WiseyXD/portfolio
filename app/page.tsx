73f1ab3ff7d5a4974221b23c20ca845269f91b97
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-11-22 23:18:20 +0530

    chores : cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>aryan's portfolio will be here </div>
+    <div>aryan's portfolio will be here. </div>
   );
 }
