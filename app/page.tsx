cd339f8b8f6853867812e5d73fe6e82553851bac
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-11-25 22:52:51 +0530

    chores : done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>aryan's portfolio will be here , firstly lets learn all the vim shortcuts and keybinds.</div>
+    <div className='font-bold'>aryan's portfolio will be here , firstly lets learn all the vim shortcuts and keybinds.</div>
   );
 }
