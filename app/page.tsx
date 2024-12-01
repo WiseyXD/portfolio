2b68eaeb1dc3bace2930d6ebc717b0e2cb3b7fef
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-12-01 11:10:45 +0530

    chores : ui packages installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div className='font-bold'>aryan portfolio will be here , sevalla is great for deployement.</div>
+    <div className='font-bold'>
+
+    </div>
   );
 }
