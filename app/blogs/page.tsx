37529f422374cbe9816dfc602dd1a1411e11423a
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-12-02 12:22:55 +0530

    chores : cleanup

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+
+export default function Blogs() {
   return (
     <main className="container relative mx-auto max-w-3xl px-5 bg-pink-200">
       <section className='mx-auto w-full bg-green-500'>
-        aryan was here.
+        aryan was here for blogs.
       </section>
     </main>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+
+
+export default function Projects() {
   return (
     <main className="container relative mx-auto max-w-3xl px-5 bg-pink-200">
       <section className='mx-auto w-full bg-green-500'>
-        aryan was here.
+        aryan was here for blogs.
       </section>
     </main>
   );
