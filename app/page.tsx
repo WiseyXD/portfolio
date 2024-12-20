421020d630053bab10fd2510d7e82629f8b39a0f
aryan.s.nag@gmail.com <aryan@explainx.ai>
2024-12-20 21:23:09 +0530

    test 3

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="container relative mx-auto max-w-3xl px-5 bg-pink-200">
       <section className='mx-auto w-full bg-green-500'>
-        asldfjlasdjflkasdfj need a project idea and a design to create the portfolio here.
+        fad need a project idea and a design to create the portfolio here.
       </section>
     </main>
   );
