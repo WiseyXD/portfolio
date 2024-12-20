feb1c12f493b3ca8ec3f40e996bd16641c753a22
aryan.s.nag@gmail.com <aryan@explainx.ai>
2024-12-20 21:18:15 +0530

    chore : mac repo setup done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="container relative mx-auto max-w-3xl px-5 bg-pink-200">
       <section className='mx-auto w-full bg-green-500'>
-        aryan need a project idea and a design to create the portfolio here.
+        aryayra need a project idea and a design to create the portfolio here.
       </section>
     </main>
   );
