db040dc31bca13bd928277149cda86b78086b3b4
aryan.s.nag@gmail.com <aryan@explainx.ai>
2025-01-18 11:01:28 +0530

    test

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,7 +3,7 @@ export default function Blogs() {
   return (
     <main className="container relative mx-auto max-w-3xl px-5 bg-pink-200">
       <section className='mx-auto w-full bg-green-500'>
-        aryan was here for blogs.
+        aryan wass here for blogs.
       </section>
     </main>
   );
