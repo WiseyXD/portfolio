4f8ab555b29b6c5e69f57b6689f5a0098ac555f4
WiseyXD <aryan.s.nag@gmail.com>
2026-04-13 21:46:57 +0200

    chore : hackathon data added

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -49,7 +49,7 @@ export function HackathonCard({
         <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
           {links?.map((link, idx) => (
             <Link href={link.href} key={idx}>
-              <Badge key={idx} title={link.title} className="flex gap-2">
+              <Badge key={idx} title={link.title} className={`flex gap-2 ${link.title === 'Victory' ? 'bg-yellow-600' : ''}`}>
                 {link.icon}
                 {link.title}
               </Badge>
--- src/components/hackathon.tsx
+++ src/components/hackathon.tsx
@@ -19,7 +19,7 @@ export default function HackathonComponent() {
               </h2>
               <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 During my time in university, I attended{" "}
-                {DATA.hackathons.length}+ hackathons. People from around the
+                {DATA.hackathons.length - 1}+ hackathons. People from around the
                 country would come together and build incredible things in 2-3
                 days. It was eye-opening to see the endless possibilities
                 brought to life by a group of motivated and passionate
